2c5c086374c689262dd303fd1d3ce63a3daf62ca
Tyreeze <wedding5@msu.edu>
2021-11-18 10:31:25 -0600

    add card background image and style artist image

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-// https://musicbrainz.org/ws/2/artist/9fff2f8a-21e6-47de-a2b8-7f449929d43f/relationships?fmt=json&inc=url-rels
-let musicbrainzArtistId = "9fff2f8a-21e6-47de-a2b8-7f449929d43f"; // Drake
-let musicbrainzUrl = "https://musicbrainz.org/ws/2/artist/" + musicbrainzArtistId + "/relationships?fmt=json&inc=url-rels";
-console.log(musicbrainzUrl);
-
-// https://api.discogs.com/database/search?artist_id=151199&key=XELicaNMkoZtoErpMlbJ&secret=fsjoNmUilNcwWiMnwaHBQnpVKniTySkp
-let discogsArtistId = "151199"; // Drake
-let discogsUrl = "https://api.discogs.com/database/search?artist_id=" + discogsArtistId + "&key=XELicaNMkoZtoErpMlbJ&secret=fsjoNmUilNcwWiMnwaHBQnpVKniTySkp";
-console.log(discogsUrl);
-=======
 var searchButton = document.querySelector("#search-btn");
 var displayResults = document.querySelector("#search-result");
 var inputEl = document.querySelector("#artist");
@@ -89,4 +78,3 @@ var searchMusicInfo = function(id) {
 }
 
 searchButton.addEventListener("click", getArtistName);
->>>>>>> c184ccfbd3883b8a6c8f27f1400ac9a2bcfac832
